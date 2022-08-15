c9bd903c495d3c98bd5405708baa165c5e481237
Jean Fellipe Ferraz Campos <jeancampos@Jeans-MacBook-Pro.local>
2022-08-15 13:38:26 -0400

    fix: change from avatar to avatar_url

--- controllers/users.js
+++ controllers/users.js
@@ -3,115 +3,123 @@ const Appointment = require('../models/appointment');
 const { hash } = require('bcryptjs');
 
 function list(req, res) {
-    try {
-        User.find({}, function(err, user) {
-            return res.json(user)
-        })
-    } catch (error) {
-        return res.json({ message: error, success: false })
-    }    
+  try {
+    User.find({}, function (err, user) {
+      return res.json(user);
+    });
+  } catch (error) {
+    return res.json({ message: error, success: false });
+  }
 }
 
 function listOne(req, res) {
-    try {
-        User.findById(req.params.id, function(err, user) {
-            return res.json(user);
-        })
-    } catch (err) {
-        return res.json({ message: error, success: false });
-    }
+  try {
+    User.findById(req.params.id, function (err, user) {
+      return res.json(user);
+    });
+  } catch (err) {
+    return res.json({ message: error, success: false });
+  }
 }
 
 function listProviders(req, res) {
-    try {
-        User.find({ isProvider: true }, function(err, users) {
-            return res.json(users);
-        })
-    } catch (err) {
-        return res.status(500).json({ message: error, success: false })
-    }
+  try {
+    User.find({ isProvider: true }, function (err, users) {
+      return res.json(users);
+    });
+  } catch (err) {
+    return res.status(500).json({ message: error, success: false });
+  }
 }
 
 function create(req, res) {
-    try {
-        const { name, email, password, avatar, is_provider } = req.body;
-        User.findOne({ email }, async function(err, userExists) {
-            if (userExists) {
-                return res.json({ error: 'Email address already used '});
-            }
+  try {
+    const { name, email, password, avatar_url, is_provider } = req.body;
+    User.findOne({ email }, async function (err, userExists) {
+      if (userExists) {
+        return res.json({ error: 'Email address already used ' });
+      }
 
-            const hashedPassword = await hash(password, 8);
-            
-            const user = new User({
-                name,
-                email,
-                password: hashedPassword,
-                avatar,
-                isProvider: is_provider
-            });
-            user.save((err) => {
-                if (err) {
-                    return res.json({ message: err, success: false })
-                }
-                return res.json(user)
-            })
-        })      
-    } catch (err) {
-        console.log(err)
-        return res.json({ message: err, success: false })
-    }
+      const hashedPassword = await hash(password, 8);
+
+      const user = new User({
+        name,
+        email,
+        password: hashedPassword,
+        avatar_url,
+        isProvider: is_provider,
+      });
+      user.save(err => {
+        if (err) {
+          return res.json({ message: err, success: false });
+        }
+        return res.json(user);
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    return res.json({ message: err, success: false });
+  }
 }
 
 function update(req, res) {
-    try {
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, { _doc }) {
-            if (err) {
-                return res.status(500).json({ message: err, success: false });
-            }
-            const modifiedValues = req.body;
-            return res.json({
-                ..._doc,
-                ...modifiedValues
-            });
-        })
-    } catch(error) {
-        return res.status(400).json({ message: error, success: false })
-    }
+  try {
+    User.findByIdAndUpdate(req.params.id, req.body, function (err, { _doc }) {
+      if (err) {
+        return res.status(500).json({ message: err, success: false });
+      }
+      const modifiedValues = req.body;
+      return res.json({
+        ..._doc,
+        ...modifiedValues,
+      });
+    });
+  } catch (error) {
+    return res.status(400).json({ message: error, success: false });
+  }
 }
 
 function deleteUser(req, res) {
-    try {
-        Appointment.find({ "user._id": req.params.id }, function(err, appointments) {
-            if (appointments.length > 0) {
-                Appointment.deleteMany({ "user._id": req.params.id }, function(err) {
-                    User.findByIdAndDelete(req.params.id, function(err) {
-                        if (err) {
-                            return res.status(500).json({ message: err, success: false })
-                        }
-            
-                        res.json({ message: `User ${req.params.id} successfully deleted`})
-                    })
-                })
-            } else {
-                User.findByIdAndDelete(req.params.id, function(err) {
-                    if (err) {
-                        return res.status(500).json({ message: err, success: false })
-                    }
-        
-                    res.json({ message: `User ${req.params.id} successfully deleted`, success: true })
-                })
+  try {
+    Appointment.find(
+      { 'user._id': req.params.id },
+      function (err, appointments) {
+        if (appointments.length > 0) {
+          Appointment.deleteMany({ 'user._id': req.params.id }, function (err) {
+            User.findByIdAndDelete(req.params.id, function (err) {
+              if (err) {
+                return res.status(500).json({ message: err, success: false });
+              }
+
+              res.json({
+                message: `User ${req.params.id} successfully deleted`,
+              });
+            });
+          });
+        } else {
+          User.findByIdAndDelete(req.params.id, function (err) {
+            if (err) {
+              return res.status(500).json({ message: err, success: false });
             }
-        })
-    } catch(error) {
-        return res.status(500).json({ message: error, success: false })
-    }
+
+            res.json({
+              message: `User ${req.params.id} successfully deleted`,
+              success: true,
+            });
+          });
+        }
+      },
+    );
+  } catch (error) {
+    return res.status(500).json({ message: error, success: false });
+  }
 }
 
 module.exports = {
-    list,
-    listOne,
-    listProviders,
-    create,
-    update,
-    deleteUser
-}
+  list,
+  listOne,
+  listProviders,
+  create,
+  update,
+  deleteUser,
+};
